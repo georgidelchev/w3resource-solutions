6a23a38b26de79b42cd7617dad4c91c4e0fbe43d
georgidelchev <alienguymc@gmail.com>
2020-04-27 22:22:43 +0300

    add files via upload

---
+++ 01 - [C# Basic Exercises]/04 - [Solution 4]/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Solution2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int resultOne = -1 + 4 * 6;
+            int resultTwo = (35 + 5) % 7;
+            int resultThree = 14 + (-4 * 6 / 11);
+            int resultFour = 2 + 15 / 6 * 1 - 7 % 2;
+
+            Console.WriteLine(resultOne);
+            Console.WriteLine(resultTwo);
+            Console.WriteLine(resultThree);
+            Console.WriteLine(resultFour);
+        }
+    }
+}
