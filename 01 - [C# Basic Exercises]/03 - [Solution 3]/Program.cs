0c4e4ba43914ed7f46f617a707774c1f23b55a7d
georgidelchev <alienguymc@gmail.com>
2020-04-27 22:26:18 +0300

    add files via upload

---
+++ 01 - [C# Basic Exercises]/03 - [Solution 3]/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SolutionThree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstNumber = int.Parse(Console.ReadLine());
+            double secondNumber = int.Parse(Console.ReadLine());
+
+            double dividing = firstNumber / secondNumber;
+            Console.WriteLine(dividing);
+        }
+    }
+}
