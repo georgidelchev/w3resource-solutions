21ccd60aebcf123f216f52eba858b69171cd8d5d
georgidelchev <alienguymc@gmail.com>
2020-04-27 10:42:10 +0300

    add files via upload

---
+++ 01 - [C# Basic Exercises]/01 - [Solution 1]/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace One
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = Console.ReadLine();
+            string lastName = Console.ReadLine();
+            Console.WriteLine($"Hello: {firstName} {lastName}");
+        }
+    }
+}
