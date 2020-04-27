9ca062624709b3f97af10c00725777aa1bf7c3fc
georgidelchev <alienguymc@gmail.com>
2020-04-27 22:21:09 +0300

    add files via upload

---
+++ 01 - [C# Basic Exercises]/05 - [Solution 5]/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Solution3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            int thirdNumber = firstNumber;
+            firstNumber = secondNumber;
+            secondNumber = thirdNumber;
+
+            Console.WriteLine(firstNumber);
+            Console.WriteLine(secondNumber);
+        }
+    }
+}
