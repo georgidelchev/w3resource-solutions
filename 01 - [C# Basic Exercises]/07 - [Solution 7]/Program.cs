680280e865ce4aaafef094617d962a29a00c7c9d
georgidelchev <alienguymc@gmail.com>
2020-04-28 19:35:00 +0300

    add files via upload

---
+++ 01 - [C# Basic Exercises]/07 - [Solution 7]/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SolutionSeven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstNumber = double.Parse(Console.ReadLine());
+            double secondNumber = double.Parse(Console.ReadLine());
+
+            double adding = firstNumber + secondNumber;
+            double subtracting = firstNumber - secondNumber;
+            double multiplying = firstNumber * secondNumber;
+            double dividing = firstNumber / secondNumber;
+            double modulo = firstNumber % secondNumber;
+
+            Console.WriteLine($"{firstNumber} + {secondNumber} = {adding}");
+            Console.WriteLine($"{firstNumber} - {secondNumber} = {subtracting}");
+            Console.WriteLine($"{firstNumber} * {secondNumber} = {multiplying}");
+            Console.WriteLine($"{firstNumber} / {secondNumber} = {Math.Round(dividing)}");
+            Console.WriteLine($"{firstNumber} % {secondNumber} = {modulo}");
+        }
+    }
+}
