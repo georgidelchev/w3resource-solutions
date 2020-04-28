9e0aedae2bf868cb08270cf7bcaa44707c65db19
georgidelchev <alienguymc@gmail.com>
2020-04-28 19:37:06 +0300

    add files via upload

---
+++ 01 - [C# Basic Exercises]/08 - [Solution 8]/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SolutionEight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= 10; i++)
+            {
+                int multiplying = number * i;
+                Console.WriteLine($"{number} * {i} = {multiplying}");
+            }
+        }
+    }
+}
