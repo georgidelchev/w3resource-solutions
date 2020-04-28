1ec1889b028b5837dd784ad00e6b393bd5a9b2eb
georgidelchev <alienguymc@gmail.com>
2020-04-28 19:25:39 +0300

    add files via upload

---
+++ 01 - [C# Basic Exercises]/06 - [Solution 6]/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SolutionSix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstNumber = double.Parse(Console.ReadLine());
+            double secondNumber = double.Parse(Console.ReadLine());
+            double thirdNumber = double.Parse(Console.ReadLine());
+
+            double multiplicationNumbers = firstNumber * secondNumber * thirdNumber;
+            Console.WriteLine(multiplicationNumbers);
+        }
+    }
+}
