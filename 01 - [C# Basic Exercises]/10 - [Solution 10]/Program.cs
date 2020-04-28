02b69cf85bc52c3d5b545d585e11c85c1bbaf15f
georgidelchev <alienguymc@gmail.com>
2020-04-28 19:42:58 +0300

    add files via upload

---
+++ 01 - [C# Basic Exercises]/10 - [Solution 10]/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SolutionTen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            int z = int.Parse(Console.ReadLine());
+
+            int result = (x + y) * z;
+            int secondResult = (x * y) + (y * z);
+            Console.WriteLine($"Result of specified numbers {x}, {y} and {z} is" +
+                $" {result} and x.y + y.z is {secondResult}");
+        }
+    }
+}
