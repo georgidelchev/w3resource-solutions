f69e48d6ddcf7977c22b58fdf25e9d841199c0de
georgidelchev <alienguymc@gmail.com>
2020-04-28 19:34:46 +0300

    add files via upload

---
+++ 01 - [C# Basic Exercises]/06 - [Solution 6]/ProgramV2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace SolutionSixV2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // using array and linq
+            
+            double[] numbers = Console
+                .ReadLine()
+                .Split()
+                .Select(double.Parse)
+                .ToArray();
+
+            double multiplication = 1;
+            foreach (var values in numbers)
+            {
+                multiplication *= values;
+            }
+            
+            Console.WriteLine(multiplication);
+        }
+    }
+}
