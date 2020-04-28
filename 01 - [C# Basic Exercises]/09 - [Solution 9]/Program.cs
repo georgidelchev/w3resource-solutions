94c6a38eff3fcd22def0f9fa6a7896ad283a12f7
georgidelchev <alienguymc@gmail.com>
2020-04-28 19:40:04 +0300

    add files via upload

---
+++ 01 - [C# Basic Exercises]/09 - [Solution 9]/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SolutionNine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+            int forthNumber = int.Parse(Console.ReadLine());
+
+            int sum = firstNumber + secondNumber + thirdNumber + forthNumber;
+            int average = sum / 4;
+            Console.WriteLine($"The average of " +
+                $"{firstNumber} , {secondNumber} , {thirdNumber} , {forthNumber}" +
+                $" is: {average}");
+        }
+    }
+}
