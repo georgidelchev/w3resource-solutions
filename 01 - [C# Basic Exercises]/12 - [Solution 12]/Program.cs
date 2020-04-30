c0c4070f31a5928690f63b6cd7b5bf0d71fee84d
georgidelchev <alienguymc@gmail.com>
2020-04-30 22:21:56 +0300

    add files via upload

---
+++ 01 - [C# Basic Exercises]/12 - [Solution 12]/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SolutionTwelve
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            for (int i = 0; i < 4; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    for (int j = 0; j < 4; j++)
+                    {
+                        Console.Write($"{number} ");
+                    }
+                }
+                else
+                {
+                    for (int j = 0; j < 4; j++)
+                    {
+                        Console.Write($"{number}");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
