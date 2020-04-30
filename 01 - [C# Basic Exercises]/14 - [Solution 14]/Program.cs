f9aae2b9acb24368f68ff92a49849bc19737155a
georgidelchev <alienguymc@gmail.com>
2020-04-30 22:37:43 +0300

    add files via upload

---
+++ 01 - [C# Basic Exercises]/14 - [Solution 14]/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SolutionFourteen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int deg = int.Parse(Console.ReadLine());
+            double degToKelvins = deg + 273;
+            double degToFahrenheits = deg * 18 / 10 + 32;
+
+            Console.WriteLine($"Kelvin = {degToKelvins:f0}");
+            Console.WriteLine($"Fahrenheit = {degToFahrenheits:f0}");
+        }
+    }
+}
