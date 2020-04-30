05d25a3c511cb2290adaea2730c94f8b6345fa1e
georgidelchev <alienguymc@gmail.com>
2020-04-30 22:27:18 +0300

    add files via upload

---
+++ 01 - [C# Basic Exercises]/13 - [Solution 13]/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SolutionThirteen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < 1; i++)
+            {
+                Console.WriteLine($"{number}{number}{number}");
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.WriteLine($"{number} {number}");
+                }
+            }
+            Console.WriteLine($"{number}{number}{number}");
+        }
+    }
+}
