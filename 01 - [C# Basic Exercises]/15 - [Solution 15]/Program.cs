089f74f3fcd9ebb71a2ff6b738ef9dee0e5f4145
georgidelchev <alienguymc@gmail.com>
2020-04-30 22:47:36 +0300

    add files via upload

---
+++ 01 - [C# Basic Exercises]/15 - [Solution 15]/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SolutionFifteen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "w3resource";
+
+            Console.WriteLine(removing(word, 1));
+            Console.WriteLine(removing(word, 9));
+            Console.WriteLine(removing(word, 0));
+        }
+        public static string removing(string str, int number)
+        {
+            return str.Remove(number, 1);
+        }
+    }
+}
