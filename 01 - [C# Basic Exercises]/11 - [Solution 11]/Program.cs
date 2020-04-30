73e1e42ab08566cca9458b8bbfd77fd41ceccded
georgidelchev <alienguymc@gmail.com>
2020-04-30 22:17:57 +0300

    add files via upload

---
+++ 01 - [C# Basic Exercises]/11 - [Solution 11]/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SolutionEleven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine($"You look older than {age}");
+        }
+    }
+}
