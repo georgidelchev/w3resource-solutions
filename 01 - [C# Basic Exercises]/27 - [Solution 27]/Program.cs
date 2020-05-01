64bfb6a7aaf33f9b406787d2c1cc8ec5670a001b
georgidelchev <alienguymc@gmail.com>
2020-05-01 17:08:49 +0300

    add files via upload

---
+++ 01 - [C# Basic Exercises]/27 - [Solution 27]/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SolutionTwentyseven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            string converted = number.ToString();
+
+            int sum = 0;
+            for (int i = 0; i < converted.Length; i++)
+            {
+                int digit = int.Parse(converted[i].ToString());
+                sum += digit;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
