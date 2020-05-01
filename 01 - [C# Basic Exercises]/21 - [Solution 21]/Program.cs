7ba8d35c9feab2c2b7a669178dbc79836e89e352
georgidelchev <alienguymc@gmail.com>
2020-05-01 15:47:30 +0300

    add files via upload

---
+++ 01 - [C# Basic Exercises]/21 - [Solution 21]/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SolutionTwentyOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int sum = firstNumber + secondNumber;
+            if ((firstNumber == 20 || secondNumber == 20) || (sum == 20))
+            {
+                Console.WriteLine(true);
+            }
+            else
+            {
+                Console.WriteLine(false);
+            }
+        }
+    }
+}
