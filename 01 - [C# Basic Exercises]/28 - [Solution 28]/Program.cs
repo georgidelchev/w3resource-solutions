725071eebde9e77ac1ee341bfa4b4ea11bbb7018
georgidelchev <alienguymc@gmail.com>
2020-05-01 17:16:40 +0300

    add files via upload

---
+++ 01 - [C# Basic Exercises]/28 - [Solution 28]/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SolutionTwentyeight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            string[] words = str.Split(new[] { " " }, StringSplitOptions.None);
+            string reversed = string.Empty;
+            for (int i = words.Length - 1; i >= 0; i--)
+            {
+                reversed += words[i] + " ";
+            }
+            Console.WriteLine(reversed);
+        }
+    }
+}
