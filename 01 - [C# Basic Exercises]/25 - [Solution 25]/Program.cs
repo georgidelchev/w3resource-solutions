8c16bca0070803b6d8941642899ce3d918e6492c
georgidelchev <alienguymc@gmail.com>
2020-05-01 16:38:01 +0300

    add files via upload

---
+++ 01 - [C# Basic Exercises]/25 - [Solution 25]/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SolutionTwentyfive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
