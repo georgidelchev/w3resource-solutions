2db8efebfa7e6a5716cbc5013210fed6ffd06291
georgidelchev <alienguymc@gmail.com>
2020-05-01 12:11:28 +0300

    add files via upload

---
+++ 01 - [C# Basic Exercises]/18 - [Solution 18]/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SolutionEighteen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOne = int.Parse(Console.ReadLine());
+            int numberTwo = int.Parse(Console.ReadLine());
+
+            if (numberOne < 0 && numberTwo >= 0)
+            {
+                Console.WriteLine(true);
+            }
+            else
+            {
+                Console.WriteLine(false);
+            }
+        }
+    }
+}
