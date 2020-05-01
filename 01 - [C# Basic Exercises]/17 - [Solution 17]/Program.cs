6b0ef1eea6548291ae5ce4b6647936862179eeef
georgidelchev <alienguymc@gmail.com>
2020-05-01 12:07:05 +0300

    add files via upload

---
+++ 01 - [C# Basic Exercises]/17 - [Solution 17]/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SolutionSeventeen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            string newStr = str[0] + str + str[0];
+            Console.WriteLine(newStr);
+        }
+    }
+}
