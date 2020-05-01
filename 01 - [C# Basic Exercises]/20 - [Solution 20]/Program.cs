a82c7f53581dd3667eee9c34604bc4bf57af992b
georgidelchev <alienguymc@gmail.com>
2020-05-01 12:17:57 +0300

    add files via upload

---
+++ 01 - [C# Basic Exercises]/20 - [Solution 20]/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SolutionTwenty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            if (firstNumber > secondNumber)
+            {
+                Console.WriteLine(Math.Abs((firstNumber - secondNumber) * 2));
+            }
+            else
+            {
+                Console.WriteLine(Math.Abs(firstNumber - secondNumber));
+            }
+        }
+    }
+}
