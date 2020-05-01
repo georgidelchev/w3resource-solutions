b35ccc8929bcd7519c6a7a89df48caea22cd2a2d
georgidelchev <alienguymc@gmail.com>
2020-05-01 17:25:34 +0300

    add files via upload

---
+++ 01 - [C# Basic Exercises]/29 - [Solution 29]/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace SolutionTwentynine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileInfo fileInfo = new FileInfo("C:/Users/gecata/Desktop/Screenshot_2.png");
+            Console.WriteLine(fileInfo.Length.ToString() + " bytes");
+        }
+    }
+}
