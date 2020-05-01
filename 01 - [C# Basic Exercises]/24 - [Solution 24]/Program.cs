41620174679d7e23d42baf3f53fbe44242399328
georgidelchev <alienguymc@gmail.com>
2020-05-01 16:36:40 +0300

    add files via upload

---
+++ 01 - [C# Basic Exercises]/24 - [Solution 24]/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SolutionTwentyfour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            string[] words = str.Split(new[] { " " }, StringSplitOptions.None);
+
+            string wantedWord = string.Empty;
+            int maxChars = int.MinValue;
+            foreach (string word in words)
+            {
+                if (word.Length > maxChars)
+                {
+                    maxChars = word.Length;
+                    wantedWord = word;
+                }
+            }
+            Console.WriteLine($"Longest word: {wantedWord}");
+            Console.WriteLine($"{maxChars} symbols");
+        }
+    }
+}
