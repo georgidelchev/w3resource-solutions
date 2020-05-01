347c5fffba6d4ca173c11f2602044ccc2d9956b3
georgidelchev <alienguymc@gmail.com>
2020-05-01 16:13:42 +0300

    add files via upload

---
+++ 01 - [C# Basic Exercises]/22 - [Solution 22]/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SolutionTwentyTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            if ((Math.Abs(number - 100) <= 20) || (Math.Abs(number - 200) <= 20))
+            {
+                Console.WriteLine(true);
+            }
+            else
+            {
+                Console.WriteLine(false);
+            }
+        }
+    }
+}
