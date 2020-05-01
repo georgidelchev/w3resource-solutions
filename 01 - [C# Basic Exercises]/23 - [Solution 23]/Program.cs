075b6c57c9dccd596244adcd605bc1079a6406d3
georgidelchev <alienguymc@gmail.com>
2020-05-01 16:31:16 +0300

    add files via upload

---
+++ 01 - [C# Basic Exercises]/23 - [Solution 23]/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SolutionTwentythree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine().ToLower();
+            Console.WriteLine(word);
+        }
+    }
+}
