35b59ffdb83a835f86348d0b1368981ad4f75abb
georgidelchev <alienguymc@gmail.com>
2020-05-01 12:14:08 +0300

    add files via upload

---
+++ 01 - [C# Basic Exercises]/19 - [Solution 19]/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SolutionNineteen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+            if (firstNumber == secondNumber)
+            {
+                sum = (firstNumber + secondNumber) * 3;
+            }
+            else
+            {
+                sum = firstNumber + secondNumber;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
