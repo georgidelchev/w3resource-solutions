f7c4e172b73a062e2dce22b29a5bdf7c4cadb775
georgidelchev <alienguymc@gmail.com>
2020-05-01 11:58:16 +0300

    add files via upload

---
+++ 01 - [C# Basic Exercises]/16 - [Solution 16]/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SolutionSixteen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+
+            Console.WriteLine(Changing(word));
+        }
+        public static string Changing(string str)
+        {
+            int length = str.Length;
+            return str[length - 1] + str.Substring(1, length - 2) + str[0];
+        }
+    }
+}
