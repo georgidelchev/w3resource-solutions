6e968475339e5bfe45ec760a688879857969f3f9
georgidelchev <alienguymc@gmail.com>
2020-05-01 17:29:25 +0300

    add files via upload

---
+++ 01 - [C# Basic Exercises]/30 - [Solution 30]/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+
+namespace SolutionThirty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hexadecimalNumber = Console.ReadLine();
+            int decimalNumber = int.Parse(hexadecimalNumber, NumberStyles.HexNumber);
+            Console.WriteLine($"From hexadecimalNumber: {hexadecimalNumber}");
+            Console.WriteLine($"To decimalNumber: {decimalNumber}");
+        }
+    }
+}
