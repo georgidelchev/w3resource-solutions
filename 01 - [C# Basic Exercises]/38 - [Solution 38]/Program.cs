a6fa27dcd23c8297f2c3ae32a73106093b01f406
georgidelchev <alienguymc@gmail.com>
2020-05-03 15:42:12 +0300

    add files via upload

---
+++ 01 - [C# Basic Exercises]/38 - [Solution 38]/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SolutionThirtyeight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "PHP";
+            string newStr = str[0].ToString() + str[1].ToString();
+            Console.WriteLine(newStr);
+        }
+    }
+}
