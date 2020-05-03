6fc1eea275fe3eabd27ad43187fb2b5d93c1a28a
georgidelchev <alienguymc@gmail.com>
2020-05-03 15:00:49 +0300

    add files via upload

---
+++ 01 - [C# Basic Exercises]/32 - [Solution 32]/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace SolutionThirtytwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            string[] splitted = word.Split(new[] { " " }, StringSplitOptions.None);
+
+            if (splitted.Length < 4)
+            {
+                foreach (var item in splitted)
+                {
+                    Console.Write($"{item} ");
+                }
+            }
+            else
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    Console.Write(string.Join(".", splitted[splitted.Length - 1]));
+                }
+            }
+        }
+    }
+}
