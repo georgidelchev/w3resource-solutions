bc763a5ac1859e126ad9744312e58e028f6d98db
georgidelchev <alienguymc@gmail.com>
2020-05-03 15:11:55 +0300

    add files via upload

---
+++ 01 - [C# Basic Exercises]/35 - [Solution 35]/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SolutionThirtyfive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            if (firstNumber < 100 && secondNumber > 200)
+            {
+                Console.WriteLine(true);
+            }
+            else
+            {
+                Console.WriteLine(false);
+            }
+        }
+    }
+}
