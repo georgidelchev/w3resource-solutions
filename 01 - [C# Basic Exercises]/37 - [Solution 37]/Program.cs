e8f0374f0128c85a7beef678b994e74c447cab46
georgidelchev <alienguymc@gmail.com>
2020-05-03 15:36:42 +0300

    add files via upload

---
+++ 01 - [C# Basic Exercises]/37 - [Solution 37]/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace SolutionThirtyseven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "PHP Tutorials";
+
+            if (str.Substring(1, 2) == "HP")
+            {
+                str = str.Remove(1, 2);
+                Console.WriteLine(str);
+            }
+            else
+            {
+                Console.WriteLine(str);
+            }
+        }
+    }
+}
