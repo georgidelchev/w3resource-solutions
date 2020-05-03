d21fe892b7f1939827952b02e28518630823416c
georgidelchev <alienguymc@gmail.com>
2020-05-03 15:14:36 +0300

    add files via upload

---
+++ 01 - [C# Basic Exercises]/36 - [Solution 36]/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SolutionThirtysix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            if ((firstNumber >= -10 && firstNumber <= 10) && (secondNumber >= -10 && secondNumber <= 10))
+            {
+                Console.WriteLine(true);
+            }
+            else
+            {
+                Console.WriteLine(false);
+            }
+        }
+    }
+}
