569780c9d6d89409f77bfa3edf28a66d93bf5fc7
georgidelchev <alienguymc@gmail.com>
2020-05-03 15:53:19 +0300

    add files via upload

---
+++ 01 - [C# Basic Exercises]/40 - [Solution 40]/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SolutionForty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            if (firstNumber == secondNumber)
+            {
+                Console.WriteLine(0);
+            }
+            else
+            {
+                int firstValue = Math.Abs(20 - firstNumber);
+                int secondValue = Math.Abs(20 - secondNumber);
+                if (firstValue < secondValue)
+                {
+                    Console.WriteLine(firstNumber);
+                }
+                else
+                {
+                    Console.WriteLine(secondNumber);
+                }
+            }
+        }
+    }
+}
