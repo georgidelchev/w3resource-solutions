272a36ae4e6d232861d7bde18b3abf69cb2dfc19
georgidelchev <alienguymc@gmail.com>
2020-05-03 15:08:43 +0300

    add files via upload

---
+++ 01 - [C# Basic Exercises]/34 - [Solution 34]/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SolutionThirtyfour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            if ((str.Length < 6 && str == "Hello") || (str.StartsWith("Hello") && str[5] == ' '))
+            {
+                Console.WriteLine(true);
+            }
+            else
+            {
+                Console.WriteLine(false);
+            }
+        }
+    }
+}
