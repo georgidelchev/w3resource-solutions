8e2f5b23c900dd294cf2936e3d0e542e463f3943
georgidelchev <alienguymc@gmail.com>
2020-05-03 15:49:42 +0300

    add files via upload

---
+++ 01 - [C# Basic Exercises]/39 - [Solution 39]/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SolutionThirtynine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+
+            int maxValue = Math.Max(firstNumber, Math.Max(secondNumber, thirdNumber));
+            int minValue = Math.Min(firstNumber, Math.Min(secondNumber, thirdNumber));
+            
+            Console.WriteLine(maxValue);
+            Console.WriteLine(minValue);
+        }
+    }
+}
