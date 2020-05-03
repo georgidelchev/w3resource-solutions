15675e0822000020b98582d2e3ff22bb6cfc6f02
georgidelchev <alienguymc@gmail.com>
2020-05-03 15:02:26 +0300

    add files via upload

---
+++ 01 - [C# Basic Exercises]/33 - [Solution 33]/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SolutionThirtythree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            if (number % 3 == 0 || number % 7 == 0)
+            {
+                Console.WriteLine(true);
+            }
+            else
+            {
+                Console.WriteLine(false);
+            }
+        }
+    }
+}
