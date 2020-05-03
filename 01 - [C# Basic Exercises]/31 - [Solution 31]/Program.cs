0c1d8fa8c0c055627bb34c372c7202221c42330f
georgidelchev <alienguymc@gmail.com>
2020-05-03 14:18:00 +0300

    add files via upload

---
+++ 01 - [C# Basic Exercises]/31 - [Solution 31]/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace SolutionThirtyone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] firstArray = Console.
+                ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] secondArray = Console.
+                ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            for (int i = 0; i < firstArray.Length; i++)
+            {
+                int sum = 0;
+                sum = firstArray[i] * secondArray[i];
+                Console.Write($"{sum} ");
+            }
+        }
+    }
+}
