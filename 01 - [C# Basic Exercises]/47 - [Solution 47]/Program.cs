381ef115110799191731174323559ac2cf5e1eb1
georgidelchev <alienguymc@gmail.com>
2020-05-04 19:06:14 +0300

    add files via upload

---
+++ 01 - [C# Basic Exercises]/47 - [Solution 47]/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace SolutionFortyseven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console
+                .ReadLine()
+                .Split(",")
+                .Select(int.Parse)
+                .ToArray();
+
+            Console.WriteLine(arr.Sum());
+        }
+    }
+}
