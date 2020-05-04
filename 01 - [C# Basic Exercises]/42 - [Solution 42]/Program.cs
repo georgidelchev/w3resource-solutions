4a16404a1a5466d982582907bbe773643a1387fb
georgidelchev <alienguymc@gmail.com>
2020-05-04 17:33:40 +0300

    add files via upload

---
+++ 01 - [C# Basic Exercises]/42 - [Solution 42]/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SolutionThirtytwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            string newWord = string.Empty;
+            if (word.Length < 4)
+            {
+                Console.WriteLine(word.ToUpper());
+            }
+            else
+            {
+                for (int i = 0; i < word.Length; i++)
+                {
+                    if (i >= 0 && i <= 3)
+                    {
+                        newWord += word[i].ToString().ToLower();
+                    }
+                    else
+                    {
+                        newWord += word[i].ToString();
+                    }
+                }
+            }
+            Console.WriteLine(newWord);
+        }
+    }
+}
