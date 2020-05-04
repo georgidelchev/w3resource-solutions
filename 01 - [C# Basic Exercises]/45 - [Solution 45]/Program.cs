a3690856e835d8288707133b190494a40e17fb3f
georgidelchev <alienguymc@gmail.com>
2020-05-04 18:38:14 +0300

    add files via upload

---
+++ 01 - [C# Basic Exercises]/45 - [Solution 45]/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace SolutionFortyfive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console
+                .ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int containsCounter = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == 5)
+                {
+                    containsCounter++;
+                }
+            }
+            Console.WriteLine($"Number of 5 present in the said array: {containsCounter} times");
+        }
+    }
+}
