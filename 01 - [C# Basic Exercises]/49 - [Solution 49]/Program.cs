bb1b77d128e7675edf1bf8e63765aa041adca8d4
georgidelchev <alienguymc@gmail.com>
2020-05-04 19:14:25 +0300

    add files via upload

---
+++ 01 - [C# Basic Exercises]/49 - [Solution 49]/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace SolutionFortynine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] firstArr = Console
+                .ReadLine()
+                .Split(",")
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] secondArr = Console
+                .ReadLine()
+                .Split(",")
+                .Select(int.Parse)
+                .ToArray();
+
+            if (firstArr.Length >= 1 && secondArr.Length >= 1)
+            {
+                if ((firstArr[0] == firstArr[firstArr.Length - 1]) ||
+                    (secondArr[0] == secondArr[secondArr.Length - 1]))
+                {
+                    Console.WriteLine(true);
+                }
+                else
+                {
+                    Console.WriteLine(false);
+                }
+            }
+        }
+    }
+}
