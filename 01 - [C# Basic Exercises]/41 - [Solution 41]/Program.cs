5494e3bae1e5288eb7cff31adef7332adc9830b4
georgidelchev <alienguymc@gmail.com>
2020-05-04 17:29:33 +0300

    add files via upload

---
+++ 01 - [C# Basic Exercises]/41 - [Solution 41]/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SolutionFortyone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            int containsCounter = 0;
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word[i] == 'w')
+                {
+                    containsCounter++;
+                }
+            }
+
+            if (containsCounter >= 1 && containsCounter <= 3)
+            {
+                Console.WriteLine(true);
+            }
+            else
+            {
+                Console.WriteLine(false);
+            }
+        }
+    }
+}
