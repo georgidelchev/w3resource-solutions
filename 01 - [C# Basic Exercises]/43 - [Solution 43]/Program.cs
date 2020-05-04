26eac8236addcfbc7d9b7076dcacb7eaff512a6c
georgidelchev <alienguymc@gmail.com>
2020-05-04 18:29:31 +0300

    add files via upload

---
+++ 01 - [C# Basic Exercises]/43 - [Solution 43]/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SolutionFortythree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            if (word[0] == 'w' && word[1] == 'w' && word[2] == 'w')
+            {
+                Console.WriteLine(true);
+            }
+            else
+            {
+                Console.WriteLine(false);
+            }
+        }
+    }
+}
