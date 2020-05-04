d71d78f6b9e6965bb0f1f28c6980402ba5e7540d
georgidelchev <alienguymc@gmail.com>
2020-05-04 19:19:13 +0300

    add files via upload

---
+++ 01 - [C# Basic Exercises]/50 - [Solution 50]/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace SolutionFifty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console
+                .ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int first = arr[0];
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                arr[i] = arr[i + 1];
+            }
+            
+            arr[arr.Length - 1] = first;
+            Console.WriteLine(string.Join(" ", arr));
+        }
+    }
+}
