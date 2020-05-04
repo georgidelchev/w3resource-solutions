0993712f96cd03150db2b0ab395788a05a7bc227
georgidelchev <alienguymc@gmail.com>
2020-05-04 19:09:46 +0300

    add files via upload

---
+++ 01 - [C# Basic Exercises]/48 - [Solution 48]/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace SolutionFortyeight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console
+                .ReadLine()
+                .Split(",")
+                .Select(int.Parse)
+                .ToArray();
+
+            if (arr.Length >= 1)
+            {
+                if (arr[0] == arr[arr.Length - 1])
+                {
+                    Console.WriteLine(true);
+                }
+                else
+                {
+                    Console.WriteLine(false);
+                }
+            }
+        }
+    }
+}
