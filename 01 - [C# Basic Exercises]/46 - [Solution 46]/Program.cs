8f249dca82c5c1d99d6e1b2937d2ee8e531d3e80
georgidelchev <alienguymc@gmail.com>
2020-05-04 18:54:17 +0300

    add files via upload

---
+++ 01 - [C# Basic Exercises]/46 - [Solution 46]/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace SolutionFortysix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int[] arr = Console.
+                ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            if (arr[0] == number || arr[arr.Length - 1] == number)
+            {
+                Console.WriteLine(true);
+            }
+            else
+            {
+                Console.WriteLine(false);
+            }
+        }
+    }
+}
