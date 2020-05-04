8c0968743e99253e04f2471e1630a7db1764ce4f
georgidelchev <alienguymc@gmail.com>
2020-05-04 18:29:50 +0300

    add files via upload

---
+++ 01 - [C# Basic Exercises]/44 - [Solution 44]/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SolutionFortyfour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            string newWord = string.Empty;
+            for (int i = 0; i < word.Length; i += 2)
+            {
+                newWord += word[i];
+            }
+            Console.WriteLine(newWord);
+        }
+    }
+}
