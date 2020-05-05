d05a850790370e6fe06691504217ab42b6f2e86a
georgidelchev <alienguymc@gmail.com>
2020-05-05 16:58:17 +0300

    add files via upload

---
+++ 01 - [C# Basic Exercises]/55 - [Solution 55]/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace SolutionFiftyfive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Number to check: ");
+            int num = int.Parse(Console.ReadLine());
+            Console.Write("Arr elements: ");
+            int[] arr = Console
+                .ReadLine()
+                .Split(",")
+                .Select(int.Parse)
+                .ToArray();
+            int index = 0;
+            int product = arr[index] * arr[index + 1];
+            index++;
+            while (index + 1 < arr.Length)
+            {
+                product = ((arr[index] * arr[index + 1]) > product) ?
+                   (arr[index] * arr[index + 1]) :
+                    product;
+                index++;
+            }
+            Console.WriteLine(product == num);
+        }
+    }
+}
