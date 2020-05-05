b70bc12dd03c1aa3549f1ee77ea68974fdaea1d6
georgidelchev <alienguymc@gmail.com>
2020-05-05 17:48:08 +0300

    add files via upload

---
+++ 01 - [C# Basic Exercises]/58 - [Solution 58]/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace SolutionFiftyeight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console
+                .ReadLine()
+                .Split(",")
+                .Select(int.Parse)
+                .ToArray();
+            Array.Sort(arr);
+            int counter = 0;
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                counter += arr[i + 1] - arr[i] - 1;
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
