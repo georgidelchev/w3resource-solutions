c23293392eb25f32a77c21a6bede5e8fc9c18ecf
georgidelchev <alienguymc@gmail.com>
2020-05-05 16:08:35 +0300

    add files via upload

---
+++ 01 - [C# Basic Exercises]/51 - [Solution 51]/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace SolutionFiftyone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console
+                .ReadLine()
+                .Split(",")
+                .Select(int.Parse)
+                .ToArray();
+
+            if (arr[0] > arr[arr.Length - 1])
+            {
+                Console.WriteLine(arr[0]);
+            }
+            else
+            {
+                Console.WriteLine($"Highest value between first and last values of the said array: {arr[arr.Length - 1]}");
+            }
+        }
+    }
+}
