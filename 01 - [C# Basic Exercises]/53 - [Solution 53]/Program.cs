2523595f586abc3d84fe465a7d0702d306bb7478
georgidelchev <alienguymc@gmail.com>
2020-05-05 16:24:44 +0300

    add files via upload

---
+++ 01 - [C# Basic Exercises]/53 - [Solution 53]/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace SolutionFiftythree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console
+                .ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            bool flag = false;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] % 2 == 0)
+                {
+                    flag = true;
+                    Console.WriteLine(true);
+                    break;
+                }
+            }
+            if (flag == false)
+            {
+                Console.WriteLine(false);
+            }
+        }
+    }
+}
