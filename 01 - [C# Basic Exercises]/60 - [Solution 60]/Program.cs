2cfcfc418e30127bd201ac173212e1c17773b8cf
georgidelchev <alienguymc@gmail.com>
2020-05-05 17:58:50 +0300

    add files via upload

---
+++ 01 - [C# Basic Exercises]/60 - [Solution 60]/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+public class Example
+{
+    public static void Main()
+    {
+        Console.WriteLine(sumMatrix(
+                    new int[][] {
+                    new int[]{0, 2, 3, 2},
+                    new int[]{0, 6, 0, 1},
+                    new int[]{4, 0, 3, 0}
+                                            }));
+        Console.WriteLine(sumMatrix(
+                    new int[][] {
+                    new int[]{1, 2, 1, 0 },
+                    new int[]{0, 5, 0, 0},
+                    new int[]{1, 1, 3, 10 }
+                                            }));
+        Console.WriteLine(sumMatrix(
+                    new int[][] {
+                    new int[]{1, 1},
+                    new int[]{2, 2},
+                    new int[]{3, 3},
+                    new int[]{4, 4}
+                                            }));
+    }
+    public static int sumMatrix(int[][] matrix)
+    {
+        int counter = 0;
+        for (int i = 0; i < matrix[0].Length; i++)
+            for (int j = 0; j < matrix.Length && matrix[j][i] > 0; j++)
+                counter += matrix[j][i];
+
+        return counter;
+    }
+}
