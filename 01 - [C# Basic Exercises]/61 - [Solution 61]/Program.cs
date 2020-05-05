3e374cf805a8c0ded8c20901f9fb4b84f71e03f7
georgidelchev <alienguymc@gmail.com>
2020-05-05 19:30:40 +0300

    add files via upload

---
+++ 01 - [C# Basic Exercises]/61 - [Solution 61]/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+
+public class Example
+{
+    public static int[] sort_numbers(int[] array)
+    {
+
+        int[] num = array.Where(a => a != -5).OrderBy(a => a).ToArray();
+        int ctr = 0;
+
+        return array.Select(a => a >= 0 ? num[ctr++] : -5).ToArray();
+    }
+
+    public static void Main()
+    {
+        int[] arr = sort_numbers(new int[] { -5, 236, 120, 70, -5, -5, 698, 280 });
+        foreach (int value in arr)
+        {
+            Console.WriteLine(value.ToString());
+        }
+    }
+}
