b4945f8189b68dcb40a59c790a8309df96129d90
georgidelchev <alienguymc@gmail.com>
2020-05-05 16:33:56 +0300

    add files via upload

---
+++ 01 - [C# Basic Exercises]/54 - [Solution 54]/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SolutionFiftyfour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int year = int.Parse(Console.ReadLine());
+            int century = (year / 100);
+            if (year % 100 == 0)
+            {
+                century += 0;
+            }
+            else
+            {
+                century += 1;
+            }
+            Console.WriteLine(century);
+        }
+    }
+}
