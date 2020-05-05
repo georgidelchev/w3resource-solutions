211a66c7604ba4457d5fe5785698bd3fcba13d15
georgidelchev <alienguymc@gmail.com>
2020-05-05 17:37:38 +0300

    add files via upload

---
+++ 01 - [C# Basic Exercises]/57 - [Solution 57]/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace SolutionFiftyseven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console
+                .ReadLine()
+                .Split(",")
+                .Select(int.Parse)
+                .ToArray();
+
+            int maxValue = arr[0] * arr[1];
+            for (int i = 1; i <= arr.Length - 2; i++)
+            {
+                maxValue = Math.Max(maxValue, arr[i] * arr[i + 1]);
+            }
+            Console.WriteLine(maxValue);
+        }
+    }
+}
