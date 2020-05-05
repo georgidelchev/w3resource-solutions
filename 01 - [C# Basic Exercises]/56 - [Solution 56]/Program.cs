eb6e06e9434e67b0d2b8c583c06779e0fb5ae85b
georgidelchev <alienguymc@gmail.com>
2020-05-05 17:00:49 +0300

    add files via upload

---
+++ 01 - [C# Basic Exercises]/56 - [Solution 56]/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SolutionFiftysix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            string reversedWord = string.Empty;
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                reversedWord += word[i];
+            }
+
+            if (word == reversedWord)
+            {
+                Console.WriteLine(true);
+            }
+            else
+            {
+                Console.WriteLine(false);
+            }
+        }
+    }
+}
