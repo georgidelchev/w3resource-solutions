16b8087f23ccc886c57901b70262f7cb27f1ad5d
georgidelchev <alienguymc@gmail.com>
2020-05-05 17:48:24 +0300

    add files via upload

---
+++ 01 - [C# Basic Exercises]/59 - [Solution 59]/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace SolutionFiftynine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console
+                .ReadLine()
+                .Split(",")
+                .Select(int.Parse)
+                .ToArray();
+
+            int counter = 0;
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if (arr[i] >= arr[i + 1])
+                {
+                    counter++;
+                }
+                if ((arr.Length > i + 2) && (arr[i] >= arr[i + 1]))
+                {
+                    counter++;
+                }
+            }
+
+            if (counter >= 2)
+            {
+                Console.WriteLine(false);
+            }
+            else
+            {
+                Console.WriteLine(true);
+            }
+        }
+    }
+}
