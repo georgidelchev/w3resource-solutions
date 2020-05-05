acfd0b026d4a0d92a98c4621e171ba580745c97b
georgidelchev <alienguymc@gmail.com>
2020-05-05 16:21:18 +0300

    add files via upload

---
+++ 01 - [C# Basic Exercises]/52 - [Solution 52]/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+
+namespace SolutionFiftytwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] firstArr = Console
+                .ReadLine()
+                .Split(",")
+                .Select(int.Parse)
+                .ToArray();
+            int[] secondArr = Console
+                .ReadLine()
+                .Split(",")
+                .Select(int.Parse)
+                .ToArray();
+            int[] thirdArr = Console
+                .ReadLine()
+                .Split(",")
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] newArr = new int[3];
+
+
+            if ((firstArr.Length < 2) && (secondArr.Length < 2) && (thirdArr.Length < 2))
+            {
+                Console.WriteLine("No middle element");
+            }
+            else
+            {
+                int firstArrMiddle = firstArr.Length / 2;
+                int secondArrMiddle = firstArr.Length / 2;
+                int thirdArrMiddle = firstArr.Length / 2;
+
+                newArr[0] += firstArr[firstArrMiddle];
+                newArr[1] += secondArr[secondArrMiddle];
+                newArr[2] += thirdArr[thirdArrMiddle];
+            }
+
+            foreach (var item in newArr)
+            {
+                Console.Write(item + " ");
+            }
+        }
+    }
+}
