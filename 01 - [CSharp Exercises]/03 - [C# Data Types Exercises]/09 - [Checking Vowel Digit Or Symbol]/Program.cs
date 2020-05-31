5c05f8dffb80f072c87870c32b855582dc9bfd8d
georgidelchev <alienguymc@gmail.com>
2020-05-31 16:50:32 +0300

    add files via upload

---
+++ 01 - [CSharp Exercises]/03 - [C# Data Types Exercises]/09 - [Checking Vowel Digit Or Symbol]/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CheckingVowelDigitOrSymbol
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char ch = char.Parse(Console.ReadLine());
+
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+            {
+                Console.WriteLine("vowel");
+            }
+            else if (ch >= '0' && ch <= '9')
+            {
+                Console.WriteLine("number");
+            }
+            else
+            {
+                Console.WriteLine("other symbol");
+            }
+        }
+    }
+}
