f7cad20b1ed93d539cef3048555e782924982907
georgidelchev <alienguymc@gmail.com>
2020-05-31 16:49:21 +0300

    add files via upload

---
+++ 01 - [CSharp Exercises]/03 - [C# Data Types Exercises]/06 - [Display Certain Values]/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DisplayCertainValues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int y = int.Parse(Console.ReadLine());
+            for (int i = y; i <= Math.Abs(y); i++)
+            {
+                int result = (i * i) - (2 * (i)) + 1;
+                Console.WriteLine($"({i})^2 - 2*({i}) + 1 = {result}");
+            }
+        }
+    }
+}
