c9d4c46a5041c1a69b0ee675cdf7d51021b3f636
georgidelchev <alienguymc@gmail.com>
2020-05-31 16:47:33 +0300

    add files via upload

---
+++ 01 - [CSharp Exercises]/03 - [C# Data Types Exercises]/02 - [Triangle Of Number]/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TriangleOfNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int width = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            for (int i = width; i >= 0; i--)
+            {
+                for (int j = i; j >= 1; j--)
+                {
+                    if (number.ToString().Length > 1)
+                    {
+                        Console.Write($"{number} ");
+                    }
+                    else
+                    {
+                        Console.Write($"{number}");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
