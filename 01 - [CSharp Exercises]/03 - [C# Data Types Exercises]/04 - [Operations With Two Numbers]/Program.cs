c6006b1fdf5116b413f04e1b04c6f67b3f379cd7
georgidelchev <alienguymc@gmail.com>
2020-05-31 16:48:30 +0300

    add files via upload

---
+++ 01 - [CSharp Exercises]/03 - [C# Data Types Exercises]/04 - [Operations With Two Numbers]/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace OperationsWithTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            char operation = char.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            int result = 0;
+            if (operation == '+')
+            {
+                result = firstNumber + secondNumber;
+            }
+            else if (operation == '-')
+            {
+                result = firstNumber - secondNumber;
+            }
+            else if (operation == '*' || operation == 'x')
+            {
+                result = firstNumber * secondNumber;
+            }
+            else if (operation == '/')
+            {
+                result = firstNumber / secondNumber;
+            }
+            else if (operation == '%')
+            {
+                result = firstNumber % secondNumber;
+            }
+
+            Console.WriteLine($"{firstNumber} {operation} {secondNumber} = {result}");
+        }
+    }
+}
