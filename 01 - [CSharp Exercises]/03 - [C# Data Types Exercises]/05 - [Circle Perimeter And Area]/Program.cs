d3f34303130067a0014dae3fc224a576a5346f81
georgidelchev <alienguymc@gmail.com>
2020-05-31 16:49:01 +0300

    add files via upload

---
+++ 01 - [CSharp Exercises]/03 - [C# Data Types Exercises]/05 - [Circle Perimeter And Area]/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CirclePerimeterAndArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius = double.Parse(Console.ReadLine());
+            double area = Math.PI * (radius * radius);
+            double perimeter = 2 * Math.PI * radius;
+
+            Console.WriteLine($"Area: {area:f0}");
+            Console.WriteLine($"Perimeter: {perimeter:f0}");
+        }
+    }
+}
