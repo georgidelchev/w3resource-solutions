2d44834ec1aefdd9c5c96a88e029db585bdb25fa
georgidelchev <alienguymc@gmail.com>
2020-05-31 16:50:52 +0300

    add files via upload

---
+++ 01 - [CSharp Exercises]/03 - [C# Data Types Exercises]/10 - [True If Both Are Odd Or Even]/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TrueIfBothAreOddOrEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            if ((firstNumber % 2 == 0) && (secondNumber % 2 == 0))
+            {
+                Console.WriteLine(true);
+            }
+            else
+            {
+                Console.WriteLine(false);
+            }
+        }
+    }
+}
