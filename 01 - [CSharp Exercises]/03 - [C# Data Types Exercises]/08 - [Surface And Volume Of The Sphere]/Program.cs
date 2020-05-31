ca39a2a603715076d95f764421e854ae02d1b5f1
georgidelchev <alienguymc@gmail.com>
2020-05-31 16:49:59 +0300

    add files via upload

---
+++ 01 - [CSharp Exercises]/03 - [C# Data Types Exercises]/08 - [Surface And Volume Of The Sphere]/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SurfaceAndVolumeOfTheSphere
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double rad = double.Parse(Console.ReadLine());
+
+            double surface = 4 * Math.PI * Math.Pow(rad, 2);
+            double volume = 4d / 3d * Math.PI * Math.Pow(rad, 3);
+
+            Console.WriteLine($"Surface: {surface:f5}");
+            Console.WriteLine($"Volume: {volume:f5}");
+        }
+    }
+}
