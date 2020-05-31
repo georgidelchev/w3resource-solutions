29381b16df263aa63d02f14cfe68e5effdcb280d
georgidelchev <alienguymc@gmail.com>
2020-05-31 16:49:43 +0300

    add files via upload

---
+++ 01 - [CSharp Exercises]/03 - [C# Data Types Exercises]/07 - [Speed In Km And Miles Per Hour]/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SpeedInKmAndMilesPerHour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double distance = double.Parse(Console.ReadLine());
+            double timeHours = double.Parse(Console.ReadLine());
+            double timeMins = double.Parse(Console.ReadLine());
+            double timeSecs = double.Parse(Console.ReadLine());
+
+            double secondsTime = (timeHours * 3600) + (timeMins * 60) + timeSecs;
+            double mps = distance / secondsTime;
+            double kmPerHour = (distance / 1000) / (secondsTime / 3600);
+            double milesPerHour = kmPerHour / 1.609;
+
+            Console.WriteLine($"Your speed in meters/sec is {mps:f3}");
+            Console.WriteLine($"Your speed in km/h is {kmPerHour:f3}");
+            Console.WriteLine($"Your speed in miles/h is {milesPerHour:f3}");
+        }
+    }
+}
