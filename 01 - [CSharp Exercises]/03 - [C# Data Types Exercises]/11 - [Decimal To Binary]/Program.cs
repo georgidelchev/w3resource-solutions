feb56c9d8a6a30dec1ecb0801d93fcbe254e6e44
georgidelchev <alienguymc@gmail.com>
2020-05-31 16:51:22 +0300

    add files via upload

---
+++ 01 - [CSharp Exercises]/03 - [C# Data Types Exercises]/11 - [Decimal To Binary]/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DecimalToBinary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            string result = string.Empty;
+            while (number > 1)
+            {
+                int reminder = number % 2;
+                result = reminder.ToString() + result;
+                number /= 2;
+            }
+            
+            Console.WriteLine(number.ToString() + result);
+        }
+    }
+}
