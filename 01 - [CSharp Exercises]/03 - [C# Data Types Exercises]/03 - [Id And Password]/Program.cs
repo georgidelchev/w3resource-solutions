9baba83923750eaf6b5509b32f6b65d633d992aa
georgidelchev <alienguymc@gmail.com>
2020-05-31 16:48:02 +0300

    add files via upload

---
+++ 01 - [CSharp Exercises]/03 - [C# Data Types Exercises]/03 - [Id And Password]/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace IdAndPassword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("ID: ");
+            string userId = Console.ReadLine();
+
+            Console.Write("Password: ");
+            string password = Console.ReadLine();
+
+            Console.Clear();
+
+            string inputId = string.Empty;
+            string inputPassword = string.Empty;
+
+            int wrongAttemptsCount = 0;
+            do
+            {
+                Console.Write("Input ID: ");
+                inputId = Console.ReadLine();
+
+                Console.Write("Input Password: ");
+                inputPassword = Console.ReadLine();
+
+                wrongAttemptsCount++;
+                if (wrongAttemptsCount > 3)
+                {
+                    Console.WriteLine("Account blocked!");
+                    break;
+                }
+            }
+            while ((userId != inputId) || (password != inputPassword));
+        }
+    }
+}
