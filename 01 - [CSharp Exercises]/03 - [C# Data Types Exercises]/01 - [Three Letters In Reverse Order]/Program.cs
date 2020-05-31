9c138cb59327f2b751df3b985e1d71dff2b31777
georgidelchev <alienguymc@gmail.com>
2020-05-31 16:47:11 +0300

    add files via upload

---
+++ 01 - [CSharp Exercises]/03 - [C# Data Types Exercises]/01 - [Three Letters In Reverse Order]/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ThreeLettersInReverseOrder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstLetter = char.Parse(Console.ReadLine());
+            char secondLetter = char.Parse(Console.ReadLine());
+            char thirdLetter = char.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{thirdLetter} {secondLetter} {firstLetter}");
+        }
+    }
+}
