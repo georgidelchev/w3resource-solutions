088801f995f9514ad91046707ec4029f5f89584a
georgidelchev <alienguymc@gmail.com>
2020-06-04 01:12:11 +0300

    add files via upload

---
+++ 01 - [CSharp Exercises]/06 - [C# Arrays]/01 - [Store Elements In An Array And Print It]/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _01_StoreElementsInAnArrayAndPrintIt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int elements = int.Parse(Console.ReadLine());
+                arr[i] = elements;
+            }
+
+            Console.WriteLine("Elements in the array are: " + string.Join(" ", arr));
+        }
+    }
+}
