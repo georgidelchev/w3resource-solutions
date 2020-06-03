85856f68919cf741ecc32cc97be96f7c2895de34
georgidelchev <alienguymc@gmail.com>
2020-06-04 01:12:57 +0300

    add files via upload

---
+++ 01 - [CSharp Exercises]/06 - [C# Arrays]/03 - [Find The Sum Of All Elements Of The Array]/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace FindTheSumOfAllElementsOfTheArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input the number of elements to be stored in the array: ");
+            int size = int.Parse(Console.ReadLine());
+
+            int[] arr = new int[size];
+            Console.WriteLine($"Input {size} elements in the array: ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int elements = int.Parse(Console.ReadLine());
+                arr[i] = elements;
+            }
+            int sum = arr.Sum();
+
+            Console.WriteLine($"Sum of all elements stored in the array is: {sum}");
+        }
+    }
+}
