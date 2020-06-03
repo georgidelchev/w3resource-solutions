1f129e45131af154a101ba53ff876bd70bef0dec
georgidelchev <alienguymc@gmail.com>
2020-06-04 01:13:19 +0300

    add files via upload

---
+++ 01 - [CSharp Exercises]/06 - [C# Arrays]/04 - [Copy The Elements From One Array Into Another Array]/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace CopyTheElementsFromOneArrayIntoAnotherArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input the number of elements to be stored in the array: ");
+            int size = int.Parse(Console.ReadLine());
+
+            int[] arr = new int[size];
+            Console.WriteLine($"Input {size} elements in the array:");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int elements = int.Parse(Console.ReadLine());
+                arr[i] = elements;
+            }
+
+            int[] secondArr = new int[arr.Length];
+            Array.Copy(arr, secondArr, arr.Length);
+
+            Console.WriteLine();
+            Console.Write("The elements stored in the first array are: ");
+            foreach (int values in arr)
+            {
+                Console.Write(values + " ");
+            }
+
+            Console.WriteLine();
+            Console.Write("The elements copied into the second array are: ");
+            foreach (int values in secondArr)
+            {
+                Console.Write(values + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
