5bbcde5351bca6bd4105afe4618d4fb5e16bde92
georgidelchev <alienguymc@gmail.com>
2020-06-04 01:13:43 +0300

    add files via upload

---
+++ 01 - [CSharp Exercises]/06 - [C# Arrays]/05 - [Count The Total Number Of Duplicated Elements In An Array]/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace CountTheTotalNumberOfDuplicateElementsInAnArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This code is counting total duplicated , for an example you have : 1 1 2 2 3 3 ,
+            // 1 is duplicated 1 time , 2 is duplicated 1 time and 3 is duplicated 1 time , 
+            // so total duplications are 3!
+            
+            Console.Write("Input the number of elements to be stored in the array: ");
+            int size = int.Parse(Console.ReadLine());
+
+            int[] arr = new int[size];
+            Console.WriteLine($"Input {size} elements in the array:");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int elements = int.Parse(Console.ReadLine());
+                arr[i] = elements;
+            }
+
+            int counter = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        counter++;
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Total number of duplicated elements found in the array is: {counter}");
+        }
+    }
+}
