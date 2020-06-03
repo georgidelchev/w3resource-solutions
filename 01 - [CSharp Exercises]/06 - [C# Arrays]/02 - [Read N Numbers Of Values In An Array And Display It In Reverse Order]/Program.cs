5a003f7d51e8787c1b38ce243ac99c54aad078ac
georgidelchev <alienguymc@gmail.com>
2020-06-04 01:12:37 +0300

    add files via upload

---
+++ 01 - [CSharp Exercises]/06 - [C# Arrays]/02 - [Read N Numbers Of Values In An Array And Display It In Reverse Order]/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace ReadNumbersOfValuesInAnArrayAndDisplayItInReverseOrder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console
+                .ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Console.Write("Before reverse: ");
+            foreach (int values in arr)
+            {
+                Console.Write(values + " ");
+            }
+
+            Array.Reverse(arr);
+
+            Console.Write("After reverse: ");
+            foreach (int values in arr)
+            {
+                Console.Write(values + " ");
+            }
+        }
+    }
+}
