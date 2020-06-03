e2f356aad62e0e320ad69c4ee90dc5a3c41b6255
georgidelchev <alienguymc@gmail.com>
2020-06-04 01:15:27 +0300

    add files via upload

---
+++ 01 - [CSharp Exercises]/06 - [C# Arrays]/09 - [Find Maximum And Minimum Element In An Array]/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace FindMaximumAndMinimumElementInAnArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input the number of elements to be stored in the array: ");
+            int size = int.Parse(Console.ReadLine());
+
+            int[] arr = new int[size];
+            int maxElement = int.MinValue;
+            int minElement = int.MaxValue;
+            Console.WriteLine($"Input 3 elements in the array:");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int elements = int.Parse(Console.ReadLine());
+                arr[i] = elements;
+                if (arr[i] > maxElement)
+                {
+                    maxElement = arr[i];
+                }
+                if (arr[i] < minElement)
+                {
+                    minElement = arr[i];
+                }
+            }
+
+            Console.WriteLine($"Maximum element is: {maxElement}");
+            Console.WriteLine($"Minimum element is: {minElement}");
+        }
+    }
+}
