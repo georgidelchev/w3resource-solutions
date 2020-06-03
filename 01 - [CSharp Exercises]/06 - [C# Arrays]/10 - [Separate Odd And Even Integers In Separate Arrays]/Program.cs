94c5cceb67167d819d814a89759ff7ea909ade40
georgidelchev <alienguymc@gmail.com>
2020-06-04 01:15:57 +0300

    add files via upload

---
+++ 01 - [CSharp Exercises]/06 - [C# Arrays]/10 - [Separate Odd And Even Integers In Separate Arrays]/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+namespace SeparateOddAndEvenIntegersInSeparateArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input the number of elements to be stored in the array: ");
+            int size = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Input {size} elements in the array:");
+            int[] arr = new int[size];
+
+            ArrayList evens = new ArrayList();
+            ArrayList odds = new ArrayList();
+            
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int elements = int.Parse(Console.ReadLine());
+                arr[i] = elements;
+
+                if (arr[i] % 2 == 0)
+                {
+                    evens.Add(arr[i]);
+                }
+                else
+                {
+                    odds.Add(arr[i]);
+                }
+            }
+
+            Console.Write($"The Even elements are: ");
+            foreach (var item in evens)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine();
+            Console.Write($"The Odd elements are: ");
+            foreach (var item in odds)
+            {
+                Console.Write(item + " ");
+            }
+        }
+    }
+}
