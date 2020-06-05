97cbfc2d6afbc91586c5190378da350150c87734
georgidelchev <alienguymc@gmail.com>
2020-06-05 17:59:57 +0300

    add files via upload

---
+++ 01 - [CSharp Exercises]/06 - [C# Arrays]/12 - [Sort Elements Of The Array In Descending Order]/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SortElementsOfTheArrayInDescendingOrder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input the size of array: ");
+            int size = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Input 3 elements in the array:");
+            int[] arr = new int[size];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int elements = int.Parse(Console.ReadLine());
+                arr[i] = elements;
+            }
+
+            Array.Sort(arr);
+            Array.Reverse(arr);
+            Console.WriteLine("Elements of the array in sorted descending order: " + string.Join(" ", arr));
+        }
+    }
+}
