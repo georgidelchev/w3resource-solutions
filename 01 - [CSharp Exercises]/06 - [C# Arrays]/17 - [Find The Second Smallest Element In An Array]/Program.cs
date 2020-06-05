f062f8488733a2e240d067c2e6ae3096bf370956
georgidelchev <alienguymc@gmail.com>
2020-06-05 18:01:45 +0300

    add files via upload

---
+++ 01 - [CSharp Exercises]/06 - [C# Arrays]/17 - [Find The Second Smallest Element In An Array]/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace FindTheSecondSmallestElementInAnArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input the size of array: ");
+            int size = int.Parse(Console.ReadLine());
+
+            int[] arr = new int[size];
+            Console.WriteLine($"Input {size} elements in the array:");
+
+            int firstMin = int.MaxValue;
+            int secondMin = int.MaxValue;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int elements = int.Parse(Console.ReadLine());
+                arr[i] = elements;
+                if (arr[i] < firstMin)
+                {
+                    secondMin = firstMin;
+                    firstMin = arr[i];
+                }
+                else if (arr[i] < secondMin)
+                {
+                    secondMin = arr[i];
+                }
+            }
+
+            Console.WriteLine($"The Second smallest element in the array is: {secondMin}");
+        }
+    }
+}
