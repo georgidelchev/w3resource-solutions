571a28e34dcf993c4d20ac567eaae2f6e42c7fe5
georgidelchev <alienguymc@gmail.com>
2020-06-05 17:59:35 +0300

    add files via upload

---
+++ 01 - [CSharp Exercises]/06 - [C# Arrays]/11 - [Sort Elements Of Array In Ascending Order]/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SortElementsOfArrayInAscendingOrder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input the size of array: ");
+            int size = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Input 5 elements in the array: ");
+            int[] arr = new int[size];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int elements = int.Parse(Console.ReadLine());
+                arr[i] = elements;
+            }
+
+            Array.Sort(arr);
+            Console.Write("Elements of array in sorted ascending order: " + string.Join(" ", arr));
+            Console.WriteLine();
+        }
+    }
+}
