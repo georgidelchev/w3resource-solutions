28e74886b6d1b1de793939747df8ade6557d99e3
georgidelchev <alienguymc@gmail.com>
2020-06-05 18:02:04 +0300

    add files via upload

---
+++ 01 - [CSharp Exercises]/06 - [C# Arrays]/18 - [2D Array Of Size 3x3 And Print The Matrix]/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _2dArrayOfSize3x3AndPrintTheMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input elements in the matrix:");
+            int[,] matrix = new int[3, 3];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    matrix[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine();
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
