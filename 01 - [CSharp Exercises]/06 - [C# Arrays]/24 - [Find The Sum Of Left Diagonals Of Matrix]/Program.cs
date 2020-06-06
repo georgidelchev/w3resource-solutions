6bb812445213e66413d60e62c9910f689479f5d4
georgidelchev <alienguymc@gmail.com>
2020-06-06 15:34:51 +0300

    add files via upload

---
+++ 01 - [CSharp Exercises]/06 - [C# Arrays]/24 - [Find The Sum Of Left Diagonals Of Matrix]/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace FindTheSumOfLeftDiagonalsOfMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input the size of the square matrix: ");
+            int size = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[size, size];
+
+            Console.WriteLine("Input elements in the first matrix:");
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    matrix[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("The Matrix is:");
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            int sum = 0;
+            int counter = size;
+            for (int i = 0; i < size; i++)
+            {
+                counter--;
+                for (int j = 0; j < size; j++)
+                {
+                    if (j == counter)
+                    {
+                        sum += matrix[i, j];
+                    }
+                }
+            }
+
+            Console.Write($"Addition of the left Diagonal elements is: {sum}");
+        }
+    }
+}
