b94366506d61c9ce53a2527e805828f9f9e25a0a
georgidelchev <alienguymc@gmail.com>
2020-06-06 15:35:10 +0300

    add files via upload

---
+++ 01 - [CSharp Exercises]/06 - [C# Arrays]/25 - [Find Sum Of Rows An Columns Of Matrix]/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace FindSumOfRowsAnColumnsOfMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input the size of the square matrix: ");
+            int size = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Input {size} elements in the first matrix:");
+            int[,] matrix = new int[size, size];
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    matrix[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("The Matrix is:");
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            int[] rowsSum = new int[size];
+            int[] columnsSum = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    rowsSum[i] += matrix[j, i];
+                }
+            }
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    columnsSum[i] += matrix[i, j];
+                }
+            }
+
+            Console.WriteLine("The sum or rows and columns of the matrix is: ");
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.Write($"[{columnsSum[i]}]");
+                Console.WriteLine();
+            }
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write($"[{rowsSum[i]}]");
+            }
+            Console.WriteLine();
+
+
+        }
+    }
+}
